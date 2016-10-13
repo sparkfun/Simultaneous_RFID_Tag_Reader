e8d0f1efb62d346f1d0df9706b72ce51bdc07011
Nathan Seidle <nathan@sparkfun.com>
2016-10-13 14:08:15 -0600

    Library additions. Writing not yet working.

--- Examples/WriteTag/WriteTag.ino
+++ Examples/WriteTag/WriteTag.ino
@@ -12,8 +12,6 @@
   Examples:
   Just read tags
   Write a new ID to a tag
-  Write to user memory
-  Kill tag
 
 
   Arduino pin 2 to Nano RX
@@ -43,19 +41,98 @@ void setup()
   //57600 works well except for large comms like reading the freq hop table (205 bytes)
   //38400 works with freq hop table reading
   //9600 may be too slow for reading lots of tags simultaneously
-  if(setupNano(38400) == false) //Configure nano to run at 57600bps
+  if(setupNano(9600) == false) //Configure nano to run at 57600bps
   {
     Serial.println("Module failed to respond. Please check wiring.");
     while(1); //Freeze!
   }
 
-  nano.setRegion(0x0D); //Set to North America
+  int amt = 10;
+  //All of these are probably not needed
+  nano.sendMessage(0x03, 0, 0); //Get HW version
+  delay(amt);
+  nano.sendMessage(0x0C, 0, 0); //Get current prog
+  delay(amt);
+  nano.sendMessage(0x68, 0, 0); //Get power mode
+  delay(amt);
+  nano.setBaud(9600);
+  delay(amt);
+  nano.getOptionalParameters(0x01, 0x0E); //Get optional params
+  delay(amt);
+  nano.getOptionalParameters(0x01, 0x12); //Get optional params
+  delay(amt);
+  
+  uint8_t blob1[] = {0x05};
+  nano.sendMessage(0x61, blob1, sizeof(blob1)); //Get antenna port
+  delay(amt);
+  
+  nano.getOptionalParameters(0x01, 0x0C); //Get optional params
+  delay(amt);
+  nano.getOptionalParameters(0x01, 0x0D); //Get optional params
+  delay(amt);
 
+  nano.sendMessage(0x63, 0, 0); //Get tag protocol
+  delay(amt);
+  nano.sendMessage(0x67, 0, 0); //Get region
+  delay(amt);
+  nano.sendMessage(0x71, 0, 0); //Get available regions
+  delay(amt);
+
+  nano.setRegion(0x0D); //Set to North America
+  delay(amt);
+
+  uint8_t blob2[] = {0x01};
+  nano.sendMessage(0x62, blob2, sizeof(blob2)); //Get read power with limits
+  delay(amt);
   nano.setReadPower(2700);
+  delay(amt);
+  nano.sendMessage(0x62, blob2, sizeof(blob2)); //Get read power with limits
+  delay(amt);
+  nano.sendMessage(0x65, 0, 0); //Get hop table
+  delay(amt);
+  nano.sendMessage(0x62, blob2, sizeof(blob2)); //Get read power with limits
+  delay(amt);
+  nano.sendMessage(0x62, blob2, sizeof(blob2)); //Get read power with limits
+  delay(amt);
+
+  nano.getProtocolParameters(0x05, 0x10); //Get protocol params
+  delay(amt);
+  nano.getProtocolParameters(0x05, 0x11); //Get protocol params
+  delay(amt);
+  nano.getProtocolParameters(0x05, 0x02); //Get protocol params
+  delay(amt);
+  nano.getProtocolParameters(0x05, 0x00); //Get protocol params
+  delay(amt);
+  nano.getProtocolParameters(0x05, 0x01); //Get protocol params
+  delay(amt);
+  nano.getProtocolParameters(0x05, 0x12); //Get protocol params
+  delay(amt);
+
+  nano.getReadPower(); //Don't think this is needed? Read the current power setting
+  delay(amt);
+
+  nano.getOptionalParameters(0x01, 0x04); //Get optional params
+  delay(amt);
+  nano.getOptionalParameters(0x01, 0x04); //Get optional params
+  delay(amt);
+
+  nano.sendMessage(0x61, blob1, sizeof(blob1)); //Get antenna port
+  delay(amt);
+  nano.sendMessage(0x61, blob1, sizeof(blob1)); //Get antenna port
+  delay(amt);
+
+  nano.sendMessage(0x70, 0, 0); //Get available protocols
+  delay(amt);
+
+  uint8_t blob3[] = {0x00, 0x00};
+  nano.sendMessage(0x10, blob3, sizeof(blob3)); //HW version (not get, not set)
+  delay(amt);
 
   nano.sendMessage(0x2A, 0, 0); //Clear tag ID buffer
+  delay(amt);
 
   //nano.setWritePower(2700); //Don't think this is needed?
+
 
   //nano.setOptionalParameters();
   //nano.setOptionalParameters(); //Probably not needed
@@ -64,47 +141,34 @@ void setup()
   //nano.setOptionalParameters(); //Probably not needed
 
   nano.setAntennaSearchList(); //Probably not needed
-
-  nano.setAntennaPort(); //Set TX/RX antenna ports to 1
-
-  uint8_t blob3[] = {0x00, 0x00};
-  nano.sendMessage(0x10, blob3, sizeof(blob3)); //HW version (not get, not set)
-
-  nano.sendMessage(0x2A, 0, 0); //Clear tag ID buffer
-
-  nano.setAntennaSearchList(); //Probably not needed
+  delay(amt);
 
   uint8_t blob5[] = {0x00, 0x00, 0x13, 0x01, 0xF4};
   nano.sendMessage(0x22, blob5, sizeof(blob5)); //Read tag ID multiple
   if (msg[0] == ALL_GOOD) printResponse();
+  delay(amt);
   
   uint8_t blob4[] = {0x01, 0xFF, 0x00};
   nano.sendMessage(0x29, blob4, sizeof(blob4)); //Get tag ID buffer
   if (msg[0] == ALL_GOOD) printResponse();
+  delay(amt);
 
   nano.setAntennaPort(); //Set TX/RX antenna ports to 1
-
-  
-  //nano.killTag(0xAABBCCDD);
-  nano.killTag(0x00);
-  if (msg[0] == ALL_GOOD) printResponse();
-  
-  while (1);
-
-  
-  
-  delay(1000);
+  delay(amt);
 
   //const uint8_t testID[] = "Hello World!";
-  /*const uint8_t testID[] = "test";
+  //const uint8_t testID[] = "test";
+  //nano.writeID(testID, sizeof(testID) - 1); //The -1 shaves off the \0 found at the end of any string
+  uint16_t testID = 0xAABB;
+  nano.writeID(testID, sizeof(testID)); //The -1 shaves off the \0 found at the end of any string
+  if (msg[0] == ALL_GOOD) printResponse();
+  else Serial.println("Failed write");
+
+  delay(100);
+
   nano.writeID(testID, sizeof(testID) - 1); //The -1 shaves off the \0 found at the end of any string
   if (msg[0] == ALL_GOOD) printResponse();
-  else Serial.println("Failed write");*/
-
-  uint8_t myEPC[] = {0xAA, 0xBB};
-  nano.readUserData(myEPC, sizeof(myEPC));  
-  if (msg[0] == ALL_GOOD) printResponse();
-  else Serial.println("Failed read");
+  else Serial.println("Failed write");
 
   while (1);
 
