0b6960527ce303a5d2a692b7d30fe32a1e96bef4
ToniCorinne <toni.klopfenstein@sparkfun.com>
2017-02-23 18:25:49 -0700

    Squashed 'Libraries/Arduino/' changes from 09f6561..d445e0f
    d445e0f Incrementing library version
    3fe6b17 Fixed bug: don't call printArray unless debug port has been defined.
    ef7f551 Renaming of example fines
    d9fd735 Renaming of example files
    
    git-subtree-dir: Libraries/Arduino
    git-subtree-split: d445e0fc11d1f5940b91b91c6c6669cd72d339cb

--- examples/Advanced/Range_Test/Range_Test.ino
+++ examples/Advanced/Range_Test/Range_Test.ino
@@ -4,7 +4,14 @@
   Date: October 3rd, 2016
   https://github.com/sparkfun/Simultaneous_RFID_Tag_Reader
 
-  Constantly reads and outputs any tags heard
+  Constantly reads. Emits a high tone beep when a tag is detected, and a low tone beep
+  when no tags are detected. This is useful for testing the read range of your setup.
+
+  Use an external power supply at set to max read power.
+
+  Note: Humans are basically bags of water. If you hold the tag in your hand you'll
+  degrade the range significantly (you meatbag, you). Tape the tag to a rolling chair
+  or other non-metal, non-watery device.
 
   If using the Simultaneous RFID Tag Reader (SRTR) shield, make sure the serial slide
   switch is in the 'SW-UART' position
@@ -17,9 +24,23 @@ SoftwareSerial softSerial(2, 3); //RX, TX
 #include "SparkFun_UHF_RFID_Reader.h" //Library for controlling the M6E Nano module
 RFID nano; //Create instance
 
+#define BUZZER1 10
+//#define BUZZER1 0 //For testing silently
+#define BUZZER2 9
+
+boolean tagDetected; //Keeps track of when we've beeped
+long lastSeen = 0; //Tracks the time when we last detected a tag
+int counter = 0; //Tracks how many times we've read a tag
+
 void setup()
 {
   Serial.begin(115200);
+
+  pinMode(BUZZER1, OUTPUT);
+  pinMode(BUZZER2, OUTPUT);
+
+  digitalWrite(BUZZER2, LOW); //Pull half the buzzer to ground and drive the other half.
+
   while (!Serial); //Wait for the serial port to come online
 
   if (setupNano(38400) == false) //Configure nano to run at 38400bps
@@ -30,14 +51,16 @@ void setup()
 
   nano.setRegion(REGION_NORTHAMERICA); //Set to North America
 
-  nano.setReadPower(500); //5.00 dBm. Higher values may caues USB port to brown out
+  //nano.setReadPower(500); //Limited read range
+  nano.setReadPower(2700); //You'll need an external power supply for this setting
   //Max Read TX Power is 27.00 dBm and may cause temperature-limit throttling
 
-  Serial.println(F("Press a key to begin scanning for tags."));
-  while (!Serial.available()); //Wait for user to send a character
-  Serial.read(); //Throw away the user's character
-
   nano.startReading(); //Begin scanning for tags
+
+  Serial.println("Go!");
+
+  lowBeep(); //Indicate no tag found
+  tagDetected = false;
 }
 
 void loop()
@@ -46,54 +69,44 @@ void loop()
   {
     byte responseType = nano.parseResponse(); //Break response into tag ID, RSSI, frequency, and timestamp
 
-    if (responseType == RESPONSE_IS_KEEPALIVE)
+    if (responseType == RESPONSE_IS_TAGFOUND)
     {
-      Serial.println(F("Scanning"));
-    }
-    else if (responseType == RESPONSE_IS_TAGFOUND)
-    {
-      //If we have a full record we can pull out the fun bits
-      int rssi = nano.getTagRSSI(); //Get the RSSI for this tag read
+      Serial.print(F("Tag detected: "));
+      Serial.println(counter++);
 
-      long freq = nano.getTagFreq(); //Get the frequency this tag was detected at
-
-      long timeStamp = nano.getTagTimestamp(); //Get the time this was read, (ms) since last keep-alive message
-
-      byte tagEPCBytes = nano.getTagEPCBytes(); //Get the number of bytes of EPC from response
-
-      Serial.print(F(" rssi["));
-      Serial.print(rssi);
-      Serial.print(F("]"));
-
-      Serial.print(F(" freq["));
-      Serial.print(freq);
-      Serial.print(F("]"));
-
-      Serial.print(F(" time["));
-      Serial.print(timeStamp);
-      Serial.print(F("]"));
-
-      //Print EPC bytes, this is a subsection of bytes from the response/msg array
-      Serial.print(F(" epc["));
-      for (byte x = 0 ; x < tagEPCBytes ; x++)
+      if (tagDetected == false) //Beep if we've detected a new tag
       {
-        if (nano.msg[31 + x] < 0x10) Serial.print(F("0")); //Pretty print
-        Serial.print(nano.msg[31 + x], HEX);
-        Serial.print(F(" "));
+        tagDetected = true;
+        highBeep();
       }
-      Serial.print(F("]"));
+      else if (millis() - lastSeen > 250) //Beep every 250ms
+      {
+        highBeep();
+      }
+      lastSeen = millis();
 
-      Serial.println();
     }
-    else if (responseType == ERROR_CORRUPT_RESPONSE)
-    {
-      Serial.println("Bad CRC");
-    }
-    else
-    {
-      //Unknown response
-      Serial.print("Unknown error");
-    }
+  }
+
+  if (tagDetected == true && (millis() - lastSeen) > 1000)
+  {
+    Serial.println(F("No tag found..."));
+
+    tagDetected = false;
+    lowBeep();
+  }
+
+  //If user presses a key, pause the scanning
+  if (Serial.available())
+  {
+    nano.stopReading(); //Stop scanning for tags
+
+    Serial.read(); //Throw away character
+    Serial.println("Scanning paused. Press key to continue.");
+    while (!Serial.available());
+    Serial.read(); //Throw away character
+
+    nano.startReading(); //Begin scanning for tags
   }
 }
 
@@ -103,14 +116,16 @@ boolean setupNano(long baudRate)
 {
   nano.begin(softSerial); //Tell the library to communicate over software serial port
 
+  //nano.enableDebugging();
+
   //Test to see if we are already connected to a module
   //This would be the case if the Arduino has been reprogrammed and the module has stayed powered
   softSerial.begin(baudRate); //For this test, assume module is already at our desired baud rate
-  while(!softSerial); //Wait for port to open
+  while (!softSerial); //Wait for port to open
 
   //About 200ms from power on the module will send its firmware version at 115200. We need to ignore this.
-  while(softSerial.available()) softSerial.read();
-  
+  while (softSerial.available()) softSerial.read();
+
   nano.getVersion();
 
   if (nano.msg[0] == ERROR_WRONG_OPCODE_RESPONSE)
@@ -142,5 +157,17 @@ boolean setupNano(long baudRate)
   nano.setAntennaPort(); //Set TX/RX antenna ports to 1
 
   return (true); //We are ready to rock
+}
+
+void lowBeep()
+{
+  tone(BUZZER1, 130, 150); //Low C
+  //delay(150);
+}
+
+void highBeep()
+{
+  tone(BUZZER1, 2093, 150); //High C
+  //delay(150);
 }
 
